f05aedd020fef6b69f7bd24c76a027de481b458a
Tolia <toliademidov@gmail.com>
2016-07-27 23:53:34 +0300

    fix

---
+++ config/mapbox.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mapID: 'toliademidov.0npp94id',
+  accessToken: 'pk.eyJ1IjoidG9saWFkZW1pZG92IiwiYSI6ImNpcXh4YWpkYjAwNWdpNW5udW8zajd3aHQifQ.wj-Gm7n2nIUAEDfKOecrDw'
+}
--- index.js
+++ index.js
@@ -1,35 +1,58 @@
 "use strict";
 
+const amqp = require('./lib/amqp')
 const main = require('./lib/main')
+
+const userConfig = require('./config/user')
+const mapConfig = require('./config/mapbox')
+const config = {
+  debug: userConfig.debug,
+  mapBoxAuthentication: mapConfig,
+}
+
+const Pokeio = require('pokemon-go-node-api')
 const MapGenerator = require('./lib/maps/mapbox')
 const getLocations = require('./lib/geo/coordinator')
-const mapBoxAuthentication = {
-  mapID: 'toliademidov.0npp94id',
-  accessToken: 'pk.eyJ1IjoidG9saWFkZW1pZG92IiwiYSI6ImNpcXh4YWpkYjAwNWdpNW5udW8zajd3aHQifQ.wj-Gm7n2nIUAEDfKOecrDw'
+
+const baseLocation = {
+  type: 'name',
+  name: 'Russia, Moscow'
 }
 
-exports.handler = (event, context, callback) => {
+const bail = (err) => {
+  console.log('error ------------------------------------------')
+  console.error(err)
+  process.exit(1)
+}
 
-  const pok = {
-    username: event.pgoUsername,
-    password: event.pgoPassword,
-    provider: event.pgoProvider
-  }
-  const config = {
-    debug: event.debug,
-    mapBoxAuthentication: mapBoxAuthentication,
-    pok: pok
-  }
+Pokeio.playerInfo.debug = config.debug
 
-  try {
-    main(config, event.pgoLocation, (pokemons, centerLocation, scanLocations) => {
-      let locations = [centerLocation]
-      let img = MapGenerator(config, pokemons, locations)
-      callback(null, img)
+Pokeio.init(userConfig.username, userConfig.password, baseLocation, userConfig.provider, (err) => {
+  if (err) { bail(err) }
+
+  amqp.sub((uid, coords, ack) => {
+    let startAt = new Date()
+    let location = {
+      type: 'coords',
+      coords: {
+        latitude: coords.latitude,
+        longitude: coords.longitude,
+        altitude: Pokeio.playerInfo.altitude
+      }
+    }
+
+    main(location, (pokemons, currentLocation, locations) => {
+      let imgLocations = Pokeio.playerInfo.debug ? locations : [centerLocation]
+      let img = MapGenerator(config, pokemons, imgLocations)
+
+      amqp.pub({
+        uid: uid,
+        src: img,
+        time: new Date() - startAt
+      })
+
+      ack()
     })
-  }
-  catch(err) {
-    callback(err, null)
-  }
-}
+  })
 
+})
