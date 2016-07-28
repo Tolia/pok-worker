69e9b3bcf8c95e2fd7aafc84ccb76ebee4841059
Tolia <toliademidov@gmail.com>
2016-07-28 19:58:56 +0300

    typo

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ main((api) => {
     api.search(coords, (src, time) => {
       console.log(`event №${uid} - done | time: ${ time }`)
       amqp.pub({ uid: uid, src: src, time: time })
-      ack()
+      ask()
     }, (err, time) => {
       console.log(`event №${uid} - error | time: ${ time } | error: ${err}`)
       amqp.pub({ uid: uid, err: err, time: time })
-      ack()
+      ask()
     })
   })
 })
